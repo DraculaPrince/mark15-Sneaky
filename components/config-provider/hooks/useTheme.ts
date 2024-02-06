40115f5483db79cd85480c6b546a9dab7adf0cc4
MadCcc <madccc@foxmail.com>
2024-02-06 17:38:03 +0800

    fix: inherit should not affect hashed and cssVar (#47360)
    * fix: inherit should not affect hashed and cssVar
    
    * chore: update
    
    * chore: add test case

--- components/config-provider/hooks/useTheme.ts
+++ components/config-provider/hooks/useTheme.ts
@@ -21,7 +21,13 @@ export default function useTheme(
 
   const themeConfig = theme || {};
   const parentThemeConfig: ThemeConfig =
-    themeConfig.inherit === false || !parentTheme ? defaultConfig : parentTheme;
+    themeConfig.inherit === false || !parentTheme
+      ? {
+          ...defaultConfig,
+          hashed: parentTheme?.hashed ?? defaultConfig.hashed,
+          cssVar: parentTheme?.cssVar,
+        }
+      : parentTheme;
 
   const themeKey = useThemeKey();
 
